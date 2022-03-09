a331039b179a503921454c7ec5bfe01876d9b2e6
Ivylove\mahaisong <mahaisong@hotmail.com>
2022-03-09 12:03:59 +0800

    init

---
+++ test/ConsoleNetcore31/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleNetcore31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
